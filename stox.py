ef53b07b852143652d0af864dec35261fb3b2ef7
Obai Shaikh <obai.shaikh@gmail.com>
2022-10-24 20:23:39 +0300

    Update stox.py

--- stox.py
+++ stox.py
@@ -277,7 +277,17 @@ def instit_pie(ticker, floatShares):
     inst_df['pct'] = inst_df.Shares / floatShares
 
     fig = px.pie(inst_df, values="pct", names="Holder", title='Institutional Holders')
-
+    fig.update_layout(
+        # hoverlabel=dict(align="left", bgcolor="rgba(0,0,0,0)"),
+        template="plotly_dark",
+        margin=dict(t=0,b=0,l=0,r=0),
+        paper_bgcolor="rgba(0,0,0,0)",
+        plot_bgcolor="rgba(0,0,0,0)",
+        # showlegend=False,
+        # yaxis=dict(showgrid=False, title={"text": "Price ($)", "standoff": 10}),
+        # yaxis2=dict(showgrid=False, title={"text": "Volume", "standoff": 1.5}),
+        # xaxis=dict(showline=False, title={"font":dict(size=24), "standoff": 1.5})
+    )
     return fig
 
 
