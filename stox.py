dd2992856f7aae408eb1a03071e70be6ddc4756c
Obai Shaikh <obai.shaikh@gmail.com>
2022-10-24 21:53:41 +0300

    Update stox.py

--- stox.py
+++ stox.py
@@ -506,13 +506,13 @@ with st.container():
     opt = ticker.option_chain(exp_date)
 
     if "Call" in opt_type:
-        df=opt.calls.round()
+        df=opt.calls.round(2)
         st.dataframe(df.drop(columns=['contractSymbol', 'change','currency','contractSize',
                                              'inTheMoney','lastTradeDate'], errors='ignore'))
         st.plotly_chart(opt_scatter(df), use_container_width=True)
 
     else:
-        df=opt.puts.round()
+        df=opt.puts.round(2)
         st.dataframe(df)
         st.plotly_chart(opt_scatter(df), use_container_width=True)
 
