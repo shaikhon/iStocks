201a9678a9f80a9113f134a033c7e43306f64747
Obai Shaikh <obai.shaikh@gmail.com>
2022-10-26 19:22:03 +0300

    Update stox.py

--- stox.py
+++ stox.py
@@ -909,7 +909,7 @@ if st.checkbox("TODO:"):
 "---"
 st.info(f"This Page Automatically Reloads Every {st.session_state.rate} Seconds. "
         f"You Change The Rate Below.")
-c1,c2=st.columns([1,4])
+c1,c2=st.columns([1,2])
 st.session_state.rate = c1.number_input('Refresh Rate (seconds):', min_value=10, max_value=360, value=30,
                                        step=10, key='reload_rate')
 "---"
