ba01ea99e3350b4a322691030dc592c90309ddda
Obai Shaikh <obai.shaikh@gmail.com>
2022-10-26 12:20:06 +0300

    Update stox.py

--- stox.py
+++ stox.py
@@ -857,10 +857,12 @@ if st.checkbox("TODO:"):
 "---"
 st.info(f"This Page Automatically Reloads Every {st.session_state.rate} Seconds. "
         f"You Change The Rate Below.")
-c1,c2,c3,c4=st.columns([1,5,1,1])
+c1,c2,c3,c4,c5,c6=st.columns([1,4,1,1,1,1])
 st.session_state.rate = c1.number_input('Refresh Rate (seconds):', min_value=10, max_value=360, value=30,
                                        step=10, key='reload_rate')
-c3.image('./images/LI-In-Bug.png', use_column_width=True)
-c4.markdown("[LinkedIn](https://www.linkedin.com/in/obai-shaikh/)")
+c4.markdown(f'<p><a href="https://www.linkedin.com/in/obai-shaikh/"> <img src="images/LI-In-Bug.png"></a></p>')
+c5.image('./images/LI-In-Bug.png', use_column_width=True)
+c6.markdown("[LinkedIn](https://www.linkedin.com/in/obai-shaikh/)")
+
 time.sleep(st.session_state.rate)
 st.experimental_rerun()
