37f2b1517f6270a06101ccf47cdf4549a2c70b03
Obai Shaikh <obai.shaikh@gmail.com>
2022-10-27 14:37:20 +0300

    Update stox.py

--- stox.py
+++ stox.py
@@ -322,7 +322,8 @@ def plot_news_item(title, link, source, pub_when, thumb):
         font=dict(
             family=font,
             size=18,
-            color="rgba(0,0,0,1)",
+            color="white",
+            # color="rgba(0,0,0,1)",
         ),
         align="left",
         bordercolor='rgba(0,0,0,0)',
