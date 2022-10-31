0a01b45d7f5c612f1e23e613c791629447394e37
Obai Shaikh <obai.shaikh@gmail.com>
2022-10-31 16:16:47 +0300

    Update stox.py

--- stox.py
+++ stox.py
@@ -386,7 +386,7 @@ def google_stock_info(google_ticker):
 
 
 @st.cache(allow_output_mutation=True,
-          hash_funcs={"builtins.method_descriptor":"datetime.date.strftime"})
+          hash_funcs={datetime:date.strftime})
 def latest_short(today):
     y = datetime.strftime(today, "%Y")
     ym = datetime.strftime(today, "%Y%m")
