796b33f590f25764be52909f2b72d142a1ad36b0
Obai Shaikh <obai.shaikh@gmail.com>
2022-11-01 19:00:41 +0300

    Update stox.py

--- stox.py
+++ stox.py
@@ -522,7 +522,7 @@ def px_income(df):
         hovermode="x",
 
         template="plotly_dark",
-        height=250,
+        # height=250,
         margin=dict(t=0,b=0,l=0,r=0),
         paper_bgcolor="rgba(0,0,0,0)",
         plot_bgcolor="rgba(0,0,0,0)",
