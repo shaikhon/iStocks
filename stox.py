7a2551eb5dfa20d6760f7b6793634f472d8f5da1
Obai Shaikh <obai.shaikh@gmail.com>
2023-06-15 22:14:20 +0300

    Update stox.py

--- stox.py
+++ stox.py
@@ -618,14 +618,13 @@ def intraday_prophet(d, d_original, idict):
                          hovertemplate='<i>Volume</i>: %{y:,}<extra></extra>'
                          ), secondary_y=False)
 
-    # # plot yhat
-    # fig.add_trace(go.Scatter(mode='lines', x=x, y=d.yhat,
-    #                          # line=dict(color='rgba(255,255,255,1)', width=1),
-    #                          line=dict(color=color, width=1),
-    #                          hovertemplate='<i>Forecast</i>: $%{y:.2f}' +
-    #                                        '<br><i>Time</i>: %{x|%H:%M}<br><extra></extra>',
-    #                          showlegend=False),
-    #               secondary_y=False)
+    # plot yhat
+    fig.add_trace(go.Scatter(mode='lines', x=x, y=d.yhat,
+                             line=dict(color=color, width=1, dash='dash'),
+                             hovertemplate='<i>Forecast</i>: $%{y:.2f}' +
+                                           '<br><i>Time</i>: %{x|%H:%M}<br><extra></extra>',
+                             showlegend=False),
+                  secondary_y=False)
 
     # plot trend error bands
     upper = d.trend_upper.to_list()
