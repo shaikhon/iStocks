ca44e7dbe7cba8e7587a2a68de90e0e93ee37157
Obai Shaikh <obai.shaikh@gmail.com>
2023-06-16 18:02:21 +0300

    Update stox.py

--- stox.py
+++ stox.py
@@ -617,8 +617,9 @@ def intrawmy(d, idict, period):
         showlegend=False,
         yaxis=dict(showgrid=False, title={"font":dict(size=24),"text": "Volume", "standoff": 10}),
         yaxis2=dict(showgrid=False, title={"font":dict(size=24),"text": "Price ($USD)", "standoff": 10}),
-        xaxis=dict(showline=False,),
-        xaxis_ticklabelstep=5,
+        xaxis=dict(showline=False, ticks='outside'),
+        xaxis_ticklabelstep=4,
+        # xaxis_ticks='outside',
         # xaxis_tickformat='%d %B (%a)<br>%Y',
         
     )
